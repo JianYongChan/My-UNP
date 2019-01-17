89d60937bc83a235883facbf6cc2cd9821e3952d
lelouch <lelouch.279978955@gmail.com>
2019-01-17 22:11:06 +0800

    SIGCHLD信号处理程序 前一个版本的问题是，如果在处理该信号的时候又有新的子进程终止，产生的SIGCHLD不会排队，所以还是会产生僵死进程。这里采用waitpid和WNOHANG选项来解决这个问题。

--- my-src/Chapter-05/sigchld.c
+++ my-src/Chapter-05/sigchld.c
@@ -6,7 +6,9 @@ sig_chld(int signo)
     pid_t   pid;
     int     stat;
 
-    pid = wait(&stat);
-    printf("child %d terminated\n", pid);
+    // -1表示等待第一个终止的子进程
+    // WNOHANG用来告知内核在有尚未终止的子进程时不要阻塞
+    while ((pid = waitpid(-1, &stat, WNOHANG)) > 0)
+        printf("child %d terminated\n", pid);
     return;
 }
