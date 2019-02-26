935b461289c2a202dac66a7f164fa348e9fc6f2e
tangmou <lelouch.279978955@gmail.com>
2019-02-26 17:46:28 +0800

    unp_sctp.h for Chapter-03

---
+++ my-src/Chapter-23/unp_sctp.h
@@ -0,0 +1,49 @@
+#ifndef UNP_SCTP_H__
+#define UNP_SCTP_H__
+
+#include <sys/types.h>  /* basic system data types */
+#include <sys/socket.h> /* basic socket definations */
+#include <sys/time.h>   /* timeval{} for select() */
+#include <time.h>       /* timespece{} for pselect() */
+#include <netinet/in.h> /* sockaddr_in{} and other Internet defns */
+#include <arpa/inet.h>  /* inet(3) functions */
+#include <netinet/sctp.h>
+
+#include <errno.h>
+#include <stdio.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <netdb.h>
+#include <strings.h>    // bzero
+#include <string.h>     // memcopy
+#include <netinet/sctp.h>
+
+typedef struct sockaddr SA;
+
+#define LISTENQ 10
+
+#define SRVPORT 4399
+
+#define MAXLINE 1024
+
+#define BUFFSIZE 1024
+
+#define SRV_MAX_SCTP_STRM 10
+
+#define SCTP_PDAPI_INCR_SZ 65535
+
+#define SCTP_PDAPI_NEED_MODE_THRESHOLD 1024
+
+sctp_assoc_t sctp_address_to_associd(int sockfd, struct sockaddr *sa, socklen_t salen);
+
+int sctp_get_no_strms(int sockfd, struct sctp_sndrcvinfo *srip);
+
+void sctp_str_cli(FILE *fp, int sockfd, SA *to, socklen_t tolen);
+void sctp_str_cli_echoall(FILE *fp, int sockfd, SA *to, socklen_t tolen);
+
+char *sock_ntop(const SA *sa, socklen_t salen);
+
+struct addrinfo *
+host_serv(const char *host, const char *serv, int family, int socktype);
+
+#endif /* UNP_SCTP_H__ */
