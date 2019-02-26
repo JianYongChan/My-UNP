e842084e468c8a7c296c4f6e86b4e23a4d1a3530
tangmou <lelouch.279978955@gmail.com>
2019-02-26 10:06:40 +0800

    sctp client(from Chapter-09)

---
+++ my-src/Chapter-23/Client/sctp_str_cli_echoall.c
@@ -0,0 +1,43 @@
+#include "../unp_sctp.h"
+
+void
+sctp_str_cli_echoall(FILE *fp, int sockfd, SA *to, socklen_t tolen)
+{
+    struct sockaddr_in perr_addr;
+    struct sctp_sndrcvinfo sri;
+    char sendline[MAXLINE], recvline[MAXLINE];
+    socklen_t len;
+    int rd_sz, i, str_sz, n;
+    int msg_flags;
+
+    bzero(sendline, sizeof(sendline));
+    bzero(&sri, sizeof(sri));
+    while(fgets(sendline, MAXLINE, fp) != NULL) {
+        str_sz = strlen(sendline);
+        if (sendline[str_sz-1] == '\n') {
+            sendline[str_sz-1] = '\0';
+            str_sz--;
+        }
+        for (i = 0; i < SRV_MAX_SCTP_STRM; i++) {
+            snprintf(sendline+str_sz, sizeof(sendline)-str_sz, ".msg.%d", i);
+            n = sctp_sendmsg(sockfd, sendline, sizeof(sendline), to, tolen, 0, 0, i, 0, 0);
+            if (n < 0) {
+                perror("sctp_sendmsg error");
+                return;
+            }
+        }
+        for (i = 0; i < SRV_MAX_SCTP_STRM; i++) {
+            len = sizeof(perr_addr);
+            rd_sz = sctp_recvmsg(sockfd, recvline, MAXLINE, (SA *)&perr_addr, &len, &sri, &msg_flags);
+            if (rd_sz < 0) {
+                perror("sctp_recvmsg error");
+                return;
+            }
+            recvline[rd_sz-1] = 0;
+            printf("From str:%d seq:%d (assoc:0x%x):",
+                    sri.sinfo_stream, sri.sinfo_ssn,
+                    (unsigned int)sri.sinfo_assoc_id);
+            printf("%.*s\n", rd_sz, recvline);
+        }
+    }
+}
---
+++ my-src/Chapter-23/Client/sctpcli01.c
@@ -0,0 +1,51 @@
+#include "../unp_sctp.h"
+
+
+int
+main(int argc, char **argv)
+{
+    int sockfd;
+    struct sockaddr_in srv_addr;
+    struct sctp_event_subscribe events;
+    int echo_to_all = 0;
+
+    if (argc < 2) {
+        perror("missing host argument - use './sctpcli01 host [echo]'\n");
+        return -1;
+    }
+    if (argc > 2) {
+        printf("echoing messages to all streams\n");
+        echo_to_all = 1;
+    }
+
+    if ((sockfd = socket(AF_INET, SOCK_SEQPACKET, IPPROTO_SCTP)) < 0) {
+        perror("socket error");
+        return -1;
+    }
+
+    bzero(&srv_addr, sizeof(srv_addr));
+    srv_addr.sin_family = AF_INET;
+    srv_addr.sin_addr.s_addr = htonl(INADDR_ANY);
+    srv_addr.sin_port = htons(SRVPORT);
+    if (inet_pton(AF_INET, argv[1], &srv_addr.sin_addr) != 1) {
+        perror("inet_pton error");
+        return -1;
+    }
+
+    bzero(&events, sizeof(events));
+    events.sctp_data_io_event = 1;
+    if (setsockopt(sockfd, IPPROTO_SCTP, SCTP_EVENTS, &events, sizeof(events)) < 0) {
+        perror("setsockopt error for SCTP_EVENTS\n");
+        return -1;
+    }
+    if (echo_to_all == 0)
+        sctp_str_cli(stdin, sockfd, (SA *)&srv_addr, sizeof(srv_addr));
+    else
+        sctp_str_cli_echoall(stdin, sockfd, (SA *)&srv_addr, sizeof(srv_addr));
+
+    if (close(sockfd) < 0) {
+        perror("close error");
+        return -1;
+    }
+    return 0;
+}
